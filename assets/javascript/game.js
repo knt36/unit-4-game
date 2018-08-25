6b0b6af1e08f5456f3d2c2f34755c72da6c383b9
ChanR <chan.rahar@gmail.om>
2018-08-24 20:44:32 -0400

    created necessary folder and file

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var rng = 0;
+var win = 0;
+var lose = 0;
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+var score = 0;
+
+rng = Math.floor(Math.random()*(120-19))+19;
+console.log(rng);
